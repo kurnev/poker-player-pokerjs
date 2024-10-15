65736c396417ea139b67fdf05be83247d15e71ac
Egor Kurnev <egor.kurnev@vimcar.com>
2024-10-15 14:27:35 +0200

    errors

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,11 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
     betCallback(10);
-    console.log(JSON.stringify(gameState, null, 4));
+    console.error(JSON.stringify(gameState, null, 4));
   }
 
   public showdown(gameState: any): void {
-    console.log("showdown", JSON.stringify(gameState, null, 4));
+    console.error("showdown", JSON.stringify(gameState, null, 4));
   }
 }
 
