496b432fcaa99157d6d0678396dc934b5a21fb17
Egor Kurnev <egor.kurnev@vimcar.com>
2024-10-15 14:46:55 +0200

    rank cards logic

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,28 @@
 export class Player {
-  public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(10);
+  public async betRequest(
+    gameState: any,
+    betCallback: (bet: number) => void,
+  ): Promise<void> {
+    const url = "https://rainman.leanpoker.org/rank";
+    const cards = gameState.players.find(
+      (player: any) => player.name === "PokerJS",
+    )?.hole_cards;
+    if (!cards) {
+      betCallback(0);
+      return;
+    }
+    const result = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ cards }),
+    });
+    await result.json();
+    console.error("result", JSON.stringify(result, null, 4));
+    // @ts-ignore
+    if (result && result.rank >= 1) {
+      betCallback(50);
+    } else {
+      betCallback(0);
+    }
     console.error(JSON.stringify(gameState, null, 4));
   }
 
