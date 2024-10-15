1e42f80b5aed36b19a747457172d053c38ed4e31
Pavel Nazarchuk <pavel.nazarchuk@vimcar.com>
2024-10-15 16:41:32 +0200

    feat: add all in

--- src/Player.ts
+++ src/Player.ts
@@ -1,51 +1,48 @@
 type Card = {
-  rank: string;
-  suit: string;
-};
+  rank: string
+  suit: string
+}
 
 export class Player {
   public async betRequest(
     gameState: any,
     betCallback: (bet: number) => void,
   ): Promise<void> {
-    // const data = await this.sendRankReq(gameState);
-    // // @ts-ignore
-    // if (data && data.rank >= 1) {
-    //   betCallback(50);
-    // } else {
-    //   betCallback(0);
-    // }
-    console.error(JSON.stringify(gameState, null, 4));
-    const minimum = gameState.minimum_raise ?? 0;
-    if (minimum < 20) {
-      betCallback(minimum);
-    } else {
-      betCallback(0);
+
+    const cards = gameState.players.find(
+      (player: any) => player.name === "PokerJS",
+    )?.hole_cards || [];
+
+    const stack = gameState.players.find(
+      (player: any) => player.name === "PokerJS",
+    )?.stack || 0;
+    
+    if (cards[0] && cards[1] && stack && this.doWePlayIt(cards[0], cards[1]) ) {
+      betCallback(stack)
     }
   }
 
-  rankTwoCards(cards: Array<Card>) {}
-
-  rankAllCards(cards: Array<Card>) {}
+  doWePlayIt(cardA: Card, cardB: Card) {
+    if (cardA.rank === cardB.rank) return true
+    return false
+  }
 
   public showdown(gameState: any): void {
     console.error("showdown", JSON.stringify(gameState, null, 4));
   }
 
-  public gwenanMethod(): void {
-    // stuff
+  public goAllIn(): void {
+    
   }
 
   async sendRankReq(gameState: any): Promise<any> {
     const url = "https://rainman.leanpoker.org/rank";
     const holeCards = gameState.players.find(
       (player: any) => player.name === "PokerJS",
-    )?.hole_cards;
+    )?.hole_cards || [];
     console.error("cards", JSON.stringify({ cards: holeCards }));
 
-    const comCards = gameState.players.find(
-      (player: any) => player.name === "PokerJS",
-    )?.community_cards;
+    const comCards = gameState?.community_cards || [];
 
     const combinedCards = [...holeCards, ...comCards];
 
