2767feec5d888fbd27bc56ffa9d12653b3632204
Egor Kurnev <egor.kurnev@vimcar.com>
2024-10-15 16:25:05 +0200

    fix

--- src/Player.ts
+++ src/Player.ts
@@ -3,33 +3,20 @@ export class Player {
     gameState: any,
     betCallback: (bet: number) => void,
   ): Promise<void> {
-    const url = "https://rainman.leanpoker.org/rank";
-    const cards = gameState.players.find(
-      (player: any) => player.name === "PokerJS",
-    )?.hole_cards;
-    console.error("cards", JSON.stringify({ cards }));
-
-    let formData = new FormData();
-    formData.append("cards", JSON.stringify(cards));
-
-    if (!cards?.length) {
-      betCallback(0);
-      return;
-    }
-    const result = await fetch(url, {
-      method: "POST",
-      body: formData,
-    });
-    const data = await result.json();
-    console.error("data", data);
-    console.error("result", data);
-    // @ts-ignore
-    if (data && data.rank >= 1) {
-      betCallback(50);
+    // const data = await this.sendRankReq(gameState);
+    // // @ts-ignore
+    // if (data && data.rank >= 1) {
+    //   betCallback(50);
+    // } else {
+    //   betCallback(0);
+    // }
+    console.error(JSON.stringify(gameState, null, 4));
+    const minimum = gameState.minimum_raise ?? 0;
+    if (minimum < 20) {
+      betCallback(minimum);
     } else {
       betCallback(0);
     }
-    console.error(JSON.stringify(gameState, null, 4));
   }
 
   public showdown(gameState: any): void {
@@ -38,6 +25,34 @@ export class Player {
 
   public gwenanMethod(): void {
     // stuff
+  }
+
+  async sendRankReq(gameState: any): Promise<any> {
+    const url = "https://rainman.leanpoker.org/rank";
+    const holeCards = gameState.players.find(
+      (player: any) => player.name === "PokerJS",
+    )?.hole_cards;
+    console.error("cards", JSON.stringify({ cards: holeCards }));
+
+    const comCards = gameState.players.find(
+      (player: any) => player.name === "PokerJS",
+    )?.community_cards;
+
+    const combinedCards = [...holeCards, ...comCards];
+
+    let formData = new FormData();
+    formData.append("cards", JSON.stringify(combinedCards));
+
+    if (combinedCards.length >= 5) {
+      const result = await fetch(url, {
+        method: "POST",
+        body: formData,
+      });
+      const data = await result.json();
+      return data;
+    }
+
+    return {};
   }
 }
 
