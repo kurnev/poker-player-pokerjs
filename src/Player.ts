79407118b3b18c44e6a0b26acd6465a9b635758d
Egor Kurnev <egor.kurnev@vimcar.com>
2024-10-15 15:48:07 +0200

    console error

--- src/Player.ts
+++ src/Player.ts
@@ -7,6 +7,7 @@ export class Player {
     const cards = gameState.players.find(
       (player: any) => player.name === "PokerJS",
     )?.hole_cards;
+    console.error("cards", cards);
     if (!cards?.length) {
       betCallback(0);
       return;
@@ -16,6 +17,7 @@ export class Player {
       body: JSON.stringify({ cards }),
     });
     const data = await result.json();
+    console.log("data", data);
     console.error("result", data);
     // @ts-ignore
     if (data && data.rank >= 1) {
